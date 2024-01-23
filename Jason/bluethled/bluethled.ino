43cdd46089ea374efd4eb26318b1cef3ccba6285
veroarriola <v.arriola@ciencias.unam.mx>
2024-01-23 12:52:07 -0600

    Leer cadenas por bluetooth

---
+++ Jason/bluethled/bluethled.ino
@@ -0,0 +1,22 @@
+int estado = '';
+const int LED = 13;
+
+void setup() {
+  pinmode(LED, OUTPUT);
+  Serial.begin(9600);    // Arduino One sólo tiene un serial
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    estado = Serial.read();
+    Serial.println("Estado" + estado);
+    String teststr = Serial.readString();  //read until timeout
+    teststr.trim();                        // remove any \r \n whitespace at the end of the String
+    if (teststr == "led") {
+      Serial.println("Prende/apaga led");
+      // TODO: prender y apagar led
+    } else {
+      Serial.println("Ignorar");
+    }
+  }
+}
